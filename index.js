80de99bc91a74557b35e5f07dbfa8590044f7c22
Kyle Eisenbarger <keisenb@ksu.edu>
2018-11-01 22:49:56 -0500

    Converted to mongoDB

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 require('dotenv').load();
 const tmi = require('tmi.js')
-const uuid = require('uuid/v1');
-const cassandra = require('cassandra-driver');
-const fs = require('fs');
 const log = require('node-file-logger');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-const ssl_option = {
-    cert: fs.readFileSync("./server.crt"),
-    secureProtocol: 'TLSv1_2_method'
-};
-
-const options = {
+log.SetUserOptions({
     timeZone: 'America/Chicago',
     folderPath: './logs/',
     dateBasedFileNaming: true,
@@ -20,17 +14,15 @@ const options = {
     timeFormat: 'HH:mm:ss.SSS',
     logLevel: 'debug',
     onlyFileLogging: true
-}
+});
 
-log.SetUserOptions(options);
+const dbName = process.env.ENV;
+const mongoClient = new MongoClient(process.env.MONGO, { useNewUrlParser: true });
 
-// Note: If you set dateBasedFileNaming to false, then a log file will be created at the folder path with the provided fileName.
-// If set to true then a logfile will be created with the name <fileName> provided in the options
-
-log.SetUserOptions(options);
-
-const authProviderLocalCassandra = new cassandra.auth.PlainTextAuthProvider(process.env.CASSANDRA_USERNAME, process.env.CASSANDRA_PASSWORD);
-const cassandraClient = new cassandra.Client({ contactPoints: [process.env.CASSANDRA_CONTACT_POINT], authProvider: authProviderLocalCassandra, sslOptions: ssl_option });
+(async function () {
+    await mongoClient.connect();
+    console.log("Connected correctly to mongo server");
+  })();
 
 var channels = [process.env.CHANNEL_NAME];
 showConnectionNotices = null; // Show messages like "Connected" and "Disconnected"
@@ -43,6 +35,23 @@ clientOptions = {
 };
 client = new tmi.client(clientOptions);
 
+async function handleChat(channel, user, message, self) {
+    var userId = parseInt(user['user-id']);
+    try {
+        const db = mongoClient.db(dbName);
+
+        var val = { userid: userId, username: user.username, message: message, posted: new Date() };
+        let r = await db.collection('messages').insertOne(val);
+        assert.equal(1, r.insertedCount);
+
+      } catch (err) {
+        console.log(err.stack);
+      }
+}
+
+function chatNotice(information) {
+    console.log(information);
+}
 
 function dehash(channel) {
     return channel.replace(/^#/, '');
@@ -50,28 +59,6 @@ function dehash(channel) {
 
 function capitalize(n) {
     return n[0].toUpperCase() + n.substr(1);
-}
-
-function handleChat(channel, user, message, self) {
-    var userId = parseInt(user['user-id']);
-    const query = 'INSERT INTO prod.messages (id, message, userid, username, posted) VALUES (?, ?, ?, ?, ?)';
-    const params = [
-        uuid(),
-        message,
-        userId,
-        user.username,
-        new Date()
-    ]
-    cassandraClient.execute(query, params, { prepare: true })
-        .then(() => console.log('Success'))
-        .catch(e => {
-            console.log('error sending message to cassandra', e);
-            log.Error('Error sending message to cassandra', 'service', 'handleChat', e);
-        });
-}
-
-function chatNotice(information) {
-    console.log(information);
 }
 
 client.addListener('message', handleChat);
@@ -99,7 +86,6 @@ client.addListener('join', function (channel, username) {
         if (showConnectionNotices) chatNotice('Joined ' + capitalize(dehash(channel)));
     }
 });
-
 
 client.addListener('crash', function () {
     chatNotice('Crashed');
--- read.js
+++ read.js
@@ -2,17 +2,55 @@ require('dotenv').load();
 const cassandra = require('cassandra-driver');
 var fs = require('fs');
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const dbName = 'test';
+
 var ssl_option = {
-  cert : fs.readFileSync("./server.crt"),
+  cert: fs.readFileSync("./server.crt"),
   secureProtocol: 'TLSv1_2_method'
 };
 
 const authProviderLocalCassandra = new cassandra.auth.PlainTextAuthProvider(process.env.CASSANDRA_USERNAME, process.env.CASSANDRA_PASSWORD);
-const cassandraClient = new cassandra.Client({contactPoints: [process.env.CASSANDRA_CONTACT_POINT], authProvider: authProviderLocalCassandra, sslOptions:ssl_option});
+const client = new cassandra.Client({ contactPoints: [process.env.CASSANDRA_CONTACT_POINT], authProvider: authProviderLocalCassandra, sslOptions: ssl_option });
 
-const query = 'SELECT * FROM prod.messages';
+const mongoClient = new MongoClient(process.env.MONGO, { useNewUrlParser: true });
 
-cassandraClient.execute(query)
-  .then(e => console.log(e.rows.length));
+(async function () {
+  await mongoClient.connect();
+  console.log("Connected correctly to server");
+
+  const query = "SELECT * FROM prod.messages";
+
+  client.stream(query)
+    .on('readable', async function () {
+      // readable is emitted as soon a row is received and parsed
+      var row;
+      while (row = this.read()) {
+        try {
+          const db = mongoClient.db(dbName);
+
+          var val = { userid: row.userid, username: row.username, message: row.message, posted: row.posted };
+          let r = await db.collection('transfer').insertOne(val);
+          assert.equal(1, r.insertedCount);
+
+        } catch (err) {
+          console.log(err.stack);
+        }
 
 
+      }
+    })
+    .on('error', function (e) {
+      console.log(e);
+    })
+    .on('end', function () {
+      // emitted when all rows have been retrieved and read
+      console.log("finished");
+      // Close connection
+      mongoClient.close();
+      client.shutdown();
+    });
+
+})();
